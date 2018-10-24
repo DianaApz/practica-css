e13fa2abb8a21db14b1acdcbfa4627877df7ad9d
DianaApz <kari_dia_12@hotmail.com>
2018-10-24 14:39:45 -0500

    add css

---
+++ app.js
@@ -0,0 +1,28 @@
+// Crea una interfaz web que tenga 4 botones llamados: Primero, Segundo, Tercero y Cuarto.
+// Crea una variable con el siguiente arreglo: [‘“Brasil”, “Chile“, “México“, “Perú”’].
+// Cuando se hace clic en cada botón, debe alertar el país correspondiente a la posición correspondiente en el arreglo. Por ejemplo, hacer clic en "Segundo" debería alertar a "Chile".
+const tag = document.getElementsByTagName('button');
+console.log(tag)
+const countries = ['Brasil', 'Chile', 'México', 'Perú'];
+
+
+
+// for (let i = 0; i < tag.length; i++) {
+//     console.log(tag[i].id);
+//     tag[i].addEventListener('click', () => {
+//         if (tag[i].id == 'one') {
+//             alert(countries[0]);
+//         } else if (tag[i].id == 'two') {
+//             alert(countries[1])
+//         } else if (tag[i].id == 'three') {
+//             alert(countries[2])
+//         } else if (tag[i].id == 'four') {
+//             alert(countries[3])
+//         }
+//     })
+// }
+// Crea una interfaz web de dos columnas usando HTML y CSS.
+// Haz que el diseño sea fluido, de modo que los tamaños relativos de las columnas se mantengan cuando se cambie el tamaño de la ventana.
+// Establece un ancho mínimo y máximo para las columnas.
+// Asegúrate de que ambas columnas tengan siempre la misma altura.
+// En pantallas pequeñas, convierte el diseño en una sola columna.
